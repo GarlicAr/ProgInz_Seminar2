a5df2bf292cf99e62cb5821a93471c42a3aea2df
Arvids Ivbuls <arvidsivbuls@gmail.com>
2023-05-18 10:18:21 +0300

    Finished @Expressions

--- src/main/java/lv/venta/models/Professor.java
+++ src/main/java/lv/venta/models/Professor.java
@@ -1,8 +1,13 @@
 package lv.venta.models;
 
 import jakarta.persistence.*;
+import jakarta.validation.*;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -19,15 +24,23 @@ public class Professor {
 	@Setter(value = AccessLevel.NONE)
 	@Column(name = "professor_id")
 	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	private long idp;
 	
 	@Column(name = "professor_name")
+	@NotNull
+	@Size(min = 3, max = 15)
+	@Pattern(regexp = "[A-Z]{1}[a-z\\ ]+")
 	private String name;
 	
 	@Column(name = "professor_surname")
+	@NotNull
+	@Size(min = 3, max = 15)
+	@Pattern(regexp = "[A-Z]{1}[a-z\\ ]+")
 	private String surname;
 	
 	@Column(name = "professor_degree")
+	@NotBlank
 	private Degree degree;
 
 	public Professor(String name, String surname, Degree degree) {
